0188037ad4ac6bf073a08e66ab8d3d25a54226d5
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-27 21:45:50 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 /*
- * Write a C program that prints exactly 'with proper grammar,
- * but the outcome is a piece of art,', followed by a new line.
+ *main - Entry point
  *
- * function main - Entry point
- *
- * Return: Always 0
+ * Return: Always (0)
  */
 
 int main(void)
