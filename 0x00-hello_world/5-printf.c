0178a64cc6022b81510f7862b8fd43c13612788f
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-27 17:55:12 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /*
- * Write a C program that prints exactly 'with proper grammar, but the outcome is a piece of art,', followed by a new line.
+ * Write a C program that prints exactly 'with proper grammar,
+ * but the outcome is a piece of art,', followed by a new line.
  *
+ * main - Entry point
  *
+ * Return: Always 0
  */
 
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
