4632fe3f43df5b732d3c400350627a7378e63c2c
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-30 22:41:43 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,39 @@
 
 int main(void)
 {
-	int num;
+	int a, b, c, d, e, f, g, h, i, j;
 
-	for (num = '0' ; num <= '9' ; num++)
-{
-		putchar (num);
-	if (num < '9')
-}
-	putchar (',');
-	putchar (' ');
-}
-}
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		for (b = '1' ; b <= '9' ; b++)
+		{
+			for (c = '2' ; c <= '9' ; c++)
+			{
+				for (d = '3' ; d <= '9' ; d++)
+				{
+					for (e = '4' ; e <= '9' ; e++)
+					{
+						for (f = '5' ; f <= '9' ; f++)
+						{
+							for (g = '6' ; g <= '9' ; g++)
+							{
+								for (h = '7' ; h <= '9' ; h++)
+								{
+									for (i = '8' ; i <= '9' ; i++)
+									{
+										for (j = '9' ; j <= '9' ; j++)
+										{
+											putchar("%d %d %d %d %d %d %d %d %d %d\n", a, b,											c, d, e, f, g, h, i, j);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 	putchar ('\n');
 
 	return (0);
