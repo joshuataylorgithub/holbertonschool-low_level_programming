c60e2c914316d2e58f28c7ca6460da02df8f8107
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-30 19:42:44 -0800

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always (0)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	{
+	if (letter == 'q' || letter == 'e')
+	{
+		continue;
+	}
+		putchar('\n');
+	}
+		return (0);
+}
