bf23ec1d146863bf6e95e4d7038a66de0c7f9f1e
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-30 20:01:01 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,7 +15,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	{
-	if (letter == 'q' || letter == 'e')
+		if (letter == 'q' || letter == 'e')
 	{
 		continue;
 	}
