bf30298e43c17b0e2b2b3540bfc24c9510682ae5
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-30 21:31:57 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always (0)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		if (letter == 'q' || letter == 'e')
+	{
+		continue;
+	}
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
