d12103ce67c4f64978dbc8b4c64b73b7f04862cd
joshuataylorgithub <6136@holbertonstudents.com>
2023-01-30 20:30:08 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always (0)
+*/
+
+int main(void)
+{
+	int i = 0
+
+	for (i = 0; i <= 9; i++)
+{
+		printf("%d\n", i);
+}
+	return (0);
+}
