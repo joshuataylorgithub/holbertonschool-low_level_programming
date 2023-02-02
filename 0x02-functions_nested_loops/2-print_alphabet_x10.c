c80f435b8456e1863cdcd0e098df58be0e375b3f
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-02 08:44:10 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times;
+ *
+ * Return: void
+*/
+
+void print_alphabet_x10(void);
+{
+	int az10x;
+	int alpha;
+
+	for (az10x = 0 ; az10x < 10 ; az10x++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
+
