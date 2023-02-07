20b66b316441f74807b129ddd546fbb02de95e5e
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-06 21:55:16 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: function that checks if c is 
+ * an upper-case letter
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'z')
+	
+	{
+	return (1);
+	}
+	
+	else
+	
+	{	
+	return (0);
+	}	
+}
+
