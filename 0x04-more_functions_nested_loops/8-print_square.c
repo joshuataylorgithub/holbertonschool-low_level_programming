346ec843f0711cc7342620f980580052a89dfba1
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-09 00:05:32 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - prints a square followed by \n
+ *
+ * @size: is the size of the square
+ *
+ * Return: nothing is void
+ */
+
+void print_square(int size)
+
+{
+	int h, w;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (h = 0; h < size; h++)
+	{
+		for (w = 0; w < size; w++)
+		{
+		_putchar('#');
+		}
+
+	_putchar('\n');
+	}
+}
