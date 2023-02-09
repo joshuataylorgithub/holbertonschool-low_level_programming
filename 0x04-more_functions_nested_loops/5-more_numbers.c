292f10a3cbff07b38623879c4d40e95c9c4609ac
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-08 21:48:53 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints more numbers
+ * Return: nothing, is void
+ * external loop, count lines
+ * internal loop, count from 0-14
+ */
+
+void more_numbers(void)
+{
+	int j;
+	int i;
+
+		for (j = 0; j <= 9; j++)
+		{
+			for (i = 0; i <= 14; i++)
+			{
+				if (i > 9)
+				{
+					_putchar ((i / 10) + '0');
+				}
+					_putchar ((i % 10) + '0');
+			}
+			_putchar('\n');
+		}
+}
