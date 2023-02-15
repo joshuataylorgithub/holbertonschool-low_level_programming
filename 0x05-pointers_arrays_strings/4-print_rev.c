e6020359b658dd234dae575949f2e2cb7fed888d
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-14 21:56:42 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string
+ * @s: Pointer to a string
+ */
+
+void print_rev(char *s)
+{
+	int revcount = 0;
+
+	while (s[revcount] != '\0')
+	{
+		revcount++;
+	}
+}
