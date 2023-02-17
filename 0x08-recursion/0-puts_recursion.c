79037aa6c03435408f84d16d95b27afe5ad1de24
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-16 16:47:33 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * _puts_recursion - prints 
+ * a string and a new line
+ * @s - string indicated
+ * Return: string indicated
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	putchar('\n');
+	return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
