48c34aec90edc6425dc3d6b4250b390aa4409f6c
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-20 19:57:11 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Write a program that multiplies two numbers.
+ * Return: (0)
+ *
+ */
+
+int main()
+{
+	int x;
+	int y;
+
+	for (x = 4; y = 5;)
+	{
+		printf(x * y);
+		return (0);
+	}
+}
