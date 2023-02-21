090d5b572fd8043c72f893883ebd261a4b1cb714
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-20 20:59:24 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,11 +7,15 @@
  * Return: (0)
  */
 
-int main()
+int main(int argc, char argv[])
 {
+	if (argc != 3)
+	{
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a + b;
 
-
-
+	printf("sum of %d plus %d equals %d\n", a, b, sum);
+	}
+	return (0);
 }
-
-
