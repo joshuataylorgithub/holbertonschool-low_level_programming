caa5a21b3224d0fe7a81fd2cb300673120a6ef76
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-20 18:08:12 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Write a program that prints
+ * the number of arguments passed into it.
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+	printf("argc = %d\n", argc);
+	return (0);
+}
