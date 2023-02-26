5193260678ce9a4ea38892fa2ae5d5cd83c63d73
joshuataylorgithub <6136@holbertonstudents.com>
2023-02-25 17:37:24 -0800

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SUM(x, y) (x + y)
+
+/*
+ * main - entry point
+ * sum(x, y) - calculates the sum of x + y
+ * @x: an int
+ * @y: an int
+ * Return: (0)
+ */
+
+int main(sum x, y)
+{
+	x = 5;
+	y = 4;
+
+	printf("%d\n", SUM);
+	return (0);
+}
