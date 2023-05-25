f03d4b9ef25241febf46c47f02c5ffb0e124fadf
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-24 21:29:40 -0700

    .

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char message[] = "with proper grammar, but the outcome is a piece of art,";
+	char message[] = "with proper grammar, but the outcome is a piece of art, ";
 
 	printf("%s", message);
 	return (0);
