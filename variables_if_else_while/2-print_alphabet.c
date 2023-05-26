d81ea8cc42ec7af7a45aec488cc30d180a2f04e6
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-26 15:15:29 -0700

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+
+	putchar(az);
+	return (0);
+}
