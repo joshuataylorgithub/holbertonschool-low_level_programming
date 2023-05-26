4b1d4eeb77478563c5e3b37226b9ccda6564a782
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-26 16:57:45 -0700

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base = 0;
+
+
+	for (base = 0; base <= 10; base++)
+
+{
+	printf("%d\n", base);
+}
+
+	return (0);
+}
