347ad2521f110a71c65e012dc5b1ab92442ae1b2
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-27 20:16:57 -0700

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,14 +8,17 @@
 int main ()
 {
 	int i;
-{
-	for (i = 0; i <= 10; i++)
-	putchar('0' + i);
 
+	for (i = 0; i <= 10; i++)
+{
+	putchar('0' + i);
+}
 	for (i = 0; i <= 6; i++);
+{	
 	putchar('a' + i);
 }
 	putchar('\n');
+	
 	return (0);
 
 }
