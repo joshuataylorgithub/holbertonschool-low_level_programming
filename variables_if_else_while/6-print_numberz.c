fa3e6df19bc74cbadda3766674a7d3335ee0d405
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-27 18:34:35 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base = 0;
+
+
+	for (base = 0; base <= 9; base++)
+
+{
+	printf("%d", base);
+}
+	printf("\n");
+	return (0);
+}
