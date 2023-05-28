53ce4ba80bdd63dce8c084077b968dc9e8271b79
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-27 20:34:06 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+{
+	putchar('0' + i);
+	for (j = 0; j <= 10; j++)
+{
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + j);
+}
+}
+	putchar('\n');
+	return (0);
+}
