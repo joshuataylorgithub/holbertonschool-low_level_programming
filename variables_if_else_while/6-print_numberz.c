f3e6db581bb405a851621328e91b5ba25e10ec95
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-27 19:33:33 -0700

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,11 @@ int main(void)
 	int ascii_numbers[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 	int i;
 
-
-	for (i = 0; i <= 9; i++)
-
+	for (i = 0; i <= 9; i++);
 {
 	putchar(ascii_numbers[i]);
+}	
 	putchar('\n');
-}
 
 	return (0);
 }
