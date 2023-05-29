1bf0b39ed4f811f2fac9481c213b8dbdf3cd149c
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-28 21:55:48 -0700

    .

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void)
+
+{
+	char alpha = 'a';
+	
+	while (alpha <= 'z')
+{
+	_putchar(alpha);
+	alpha++;
+}
+	_putchar();
+}
