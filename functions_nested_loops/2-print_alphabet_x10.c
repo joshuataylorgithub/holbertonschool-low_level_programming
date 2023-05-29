3eb500dd9b458d713a9489eb393599dd79e98558
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-28 22:38:28 -0700

    .

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print_alphabet(void) - Prints alphabet followed by a new line
+ *
+ *
+ * Return: (void)
+ */
+
+void print_alphabet(void)
+
+{
+	char alpha = 'a';
+	int iteration = 0;
+
+	while (iteration < 10)
+{
+		while (alpha <= 'z')
+{
+		putchar(alpha);
+		alpha++;
+}
+
+	putchar('\n');
+	iteration++;
+
+	alpha = 'a';
+}
+}
