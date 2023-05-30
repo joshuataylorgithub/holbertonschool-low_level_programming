c98be983fac50e9e12a6c011fdbd62d4fb99e840
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-30 15:08:49 -0700

    .

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -16,23 +16,21 @@ int print_sign(int n)
 
 {
 
-	if (n > '0')
+	if (n > 0)
 {
-	putchar("+");
-}
+	putchar('+');
 	return (1);
-
-	else if (n == '0')
+}
+	else if (n == 0)
 {	
-	putchar(0);
+	putchar('0');
+	return (0);
+}
+	else if (n < 0)
+{
+	putchar('-');
+	return (-1);
 }
 	return (0);
-
-	else if (n < '0')
-{
-	putchar("-");
-}
-	return (-1);
-
 }
 
