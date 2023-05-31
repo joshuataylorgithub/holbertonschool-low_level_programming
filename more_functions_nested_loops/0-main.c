0eb237aafe8cc32243791792a43e101047e01511
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-31 16:15:30 -0700

    .

---
+++ more_functions_nested_loops/0-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	char c;
+	c = 'A';
+{	
+	printf("%c: %d\n", c, _isupper(c));
+}
+	c = 'a';
+{
+	printf("%c: %d\n", c, isupper(c));
+}
+	return (0);
+}
