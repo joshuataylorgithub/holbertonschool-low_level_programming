2f4ee8ada4f9dc1ffdea3ddf1bd6b1e208a5f6fe
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-30 21:29:13 -0700

    .

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,11 +14,15 @@
 void times_table(void)
 
 {
-	int i, result;
+	int i, j, result;
 
 	for (i = 0; i <= 10; i++)
+	for (j = 0; j <= 10; j++)
 {
-	result = 9 * i;
-	printf("9 * %d = %d\n", i, result);
+	result = i * j;
+	printf("%-3d", result);
 }
+	printf("\n");
 }
+
+
