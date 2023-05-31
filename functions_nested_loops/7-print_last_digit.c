966c9fa17319393cfeef97a600ceaa372267a215
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-30 20:13:59 -0700

    .

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ *
+ * @LD: is last digit
+ * Return: Always 0 (Success)
+ */
+
+
+int print_last_digit(int n)
+{
+	int LD;
+	LD = n % 10;
+
+	if (n)
+{
+	return (LD);
+}
+	else
+{
+	return (0);
+}
+}
