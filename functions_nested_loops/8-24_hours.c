4418318af077b378962c6ff2a756f114aca1b58f
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-30 21:08:11 -0700

    .

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * jack_bauer - prints a 24 hour clock
+ *
+ * @hour: is hours in day
+ * @minute: is minutes in hour
+ * Return: Always 0 (Success)
+ */
+
+
+void jack_bauer(void)
+{
+	int hour, minute; 
+
+	for (hour = 0; hour <= 23; hour++)
+		for (minute = 0; minute <= 59; minute++)
+{
+	printf("%02d:%02d\n", hour, minute);
+}
+
+}
