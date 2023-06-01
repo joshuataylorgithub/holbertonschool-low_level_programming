3d695c170043a59d8dbbcb7f106ad487bdd26b75
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-31 19:14:36 -0700

    .

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,14 @@
 #include <ctype.h>
 
 /**
- * print_numbers - prints numbers 0 through 9
+ * print_most_numbers - prints numbers 0 through 9
  *
  * Return: Always 0 (Success)
  */
 
 void print_most_numbers(void)
 {
-	char numbers[] = {"01356789"};
+	char numbers[] = "01356789";
 	int i;
 
 	for (i = 0; i <= 9; i++)
