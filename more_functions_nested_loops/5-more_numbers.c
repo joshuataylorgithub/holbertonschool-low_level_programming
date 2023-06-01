1713fa1c107ee90e0b01edf4ed236bea273ce1ed
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-31 20:35:34 -0700

    .

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,17 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
-	for (j = 0; j <= 10; j++)
+	for (i = 0; i <= 9; i++);
+{
+	for (j = 0; j <= 14; j++)
+{
+	if (i <= 9)
 {
 	_putchar(i + '0');
 }
 	_putchar('\n');
-
+}
+}
 }
