2830f139b6ae89a2d6cbfe964b1c49c5ecbe0158
Joshua Taylor <joshuataylor7777@gmail.com>
2023-05-31 20:07:01 -0700

    .

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -13,13 +13,10 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
+	if (i != 2 && i != 4)
 {
-	if (i != '2' && i != '4')
-{
-	putchar('0' + i);
+	putchar(i + '0');
 }
-}
-	putchar('\0');
 	putchar('\n');
 
 }
