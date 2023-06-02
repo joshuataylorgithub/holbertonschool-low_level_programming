c1179d28fed50ed1e5e9989a4519c4f899427d9d
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-01 21:44:52 -0700

    .

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - Write a function that takes a pointer
- *  to an int as parameter and updates the value 
- *  it points to to 98 
+ *  to an int as parameter and updates the value
+ *  it points to to 98
  *
  *  @n: variable being referenced
  *
