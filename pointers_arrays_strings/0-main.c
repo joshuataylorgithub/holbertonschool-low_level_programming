695f79c41b414194ff09334397f2d607d2cd56c9
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-01 21:40:11 -0700

    .

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,14 +1,25 @@
 #include <unistd.h>
 #include "main.h"
+
+void reset_to_98(int *n);
+
 /**
+ * main - check the code
  *
  *
  *
- *
+ * Return: 0
  *
  **/
 
 
+int main(void)
 {
-
+	int n;
+	
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
