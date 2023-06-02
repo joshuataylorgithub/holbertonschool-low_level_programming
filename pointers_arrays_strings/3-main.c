0f08916f8baea504a59e1d5e9df26c201ad5d8a1
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-01 23:03:02 -0700

    .

---
+++ pointers_arrays_strings/3-main.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+void _puts(char *str);
+
+/**
+ * main - checks the code
+ *
+ *
+ * return (0)
+ *
+ **/
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
