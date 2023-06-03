ccca5fdbb06849be91319ce033b0e5064b3d4883
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-03 05:58:14 -0700

    .

---
+++ pointers_arrays_strings/7-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void puts_half(char *str);
+
+/**
+ * main - checks the code
+ *
+ *
+ *
+ * Return (0)
+ **/
+
+int main(void)
+{
+	char *str;
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half - prints half the string
+ *
+ *
+ *
+ * Return: (void)
+ **/
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index;
+	int i;
+
+	if (str == NULL)
+{
+	return;
+}
+	if (length % 2 == 0)
+{
+	start_index = length / 2;
+}
+	else
+{
+	start_index = (length -1) / 2;
+}
+	for (i = start_index; i < length; i++)
+{
+	putchar(str[i]);
+}
+	putchar('\n');
+}
