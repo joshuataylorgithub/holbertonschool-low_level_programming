3296588a5e8b95fe0e000867c11dae5a08d0f453
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-05 15:13:53 -0700

    .

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -18,10 +18,13 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = (n * -1);
+		last_digit = (n % 10) * -1;
+	}
+	else
+	{
+		last_digit = n % 10;
 	}
 
-	last_digit = (n % 10);
 
 	_putchar(last_digit + '0');
 	return (last_digit);
