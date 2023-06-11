f5d1bf1a4aa3ea338dbe55ee4bb484772deb0d06
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-11 04:31:27 -0700

    .

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -1,24 +1,43 @@
-#include <unistd.h>
 #include "main.h"
-
-int _strlen(char *);
 
 /**
  * main - checks the code
- 
  *
- * Return: 0
  *
+ *
+ * Return (0)
  **/
 
 int main(void)
 {
-	char *str;
-	int len;
+	char s1[98];
+	char *ptr;
+	int i;
 
-	str = ("My first strlen!");
-	len = _strlen(str);
-	printf("%d\n", len);
+	for (i = 0; i < 98 -1; i++)
+{
+	s1[i] = '*';
+}
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	for (i = 0; i < 98; i++)
+{
+	if (i % 10)
+	{
+	printf(" ");
+	}
+	if (!(i % 10) && i)
+	{
+	printf("\n");
+	}
+	printf("0x%02x", s1[i]);
+}
+	printf("\n");
 	return (0);
-
-}	
+}
