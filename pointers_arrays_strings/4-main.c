3156ae2bdc25ab2aef62a9a2acf1e3baf8d793d7
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-11 22:42:18 -0700

    .

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,20 +1,40 @@
 #include "main.h"
 
-void print_rev(char *s);
-
 /**
- * main - checks the code
+ * reverse_array - reverses the array
  *
- *
+ * @a: array of integers
+ * @n: number of elements to swap
  *
  * Return (0)
  **/
 
+void reverse_array(int *a, int n)
+{
+	int i;
+	
+	i = 0;
+
+	while (i < n)
+{
+	if (i != 0)
+{	
+	printf(", ");
+}
+	printf("%d", a[i]);
+	i++;
+}
+	printf("/n");
+}
+
+
 int main(void)
 {
-	char *str;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
 	return (0);
+
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,8 +25,8 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, size_t n);
 int _strcmp(const char *s1, const char *s2);
-
-
+void reverse_array(int *a, int n);
+void print_array(int *a, int n);
 
 
 
