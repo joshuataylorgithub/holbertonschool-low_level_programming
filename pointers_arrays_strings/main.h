23c8ef84a90333b15ecb59e8d1f42a065213d2fe
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-12 23:07:01 -0700

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,6 +32,7 @@ char *cap_string(char *str);
 char *leet(char *);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 
