d762aac50cc1622fa5e94b96b21f30e4103ce335
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-24 16:26:29 -0700

    0

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,7 +10,34 @@
 
 int main(int argc, char *argv[])
 {
+	int sum = 0;
+	int i, j;
+	int num;
 
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+		if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+		}
+		num = atoi(argv[i]);
+		if (num > 0)
+		{
+		sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 
 }
 
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]);
 
