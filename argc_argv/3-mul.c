3dfa004e009c33f0e80441767969a94a62a7e2b6
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-24 15:02:34 -0700

    1

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - write a function that multiplies two numbers
+ *
+ * @argc: argument count
+ * @argv: argument variables
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+	
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - write a program that adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument variable
+ *
+ * Return (0) or (1) if error
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+}
+
