2b6cf9e2bf9989bdf8bec9ffbc6f04eab84b917e
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-24 13:56:52 -0700

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ *
+ * @arcg: argument count
+ * @argv: arguments
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
