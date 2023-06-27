612459def915403c1f537ecaf6b44d4147abe660
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-26 21:53:33 -0700

    3

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,8 +9,8 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t length_s1 = strlen(s1);
-	size_t length_s2 = strlen(s2);
+	size_t length_s1 = (s1 != NULL) ? strlen(s1) : 0;
+	size_t length_s2 = (s2 != NULL) ? strlen(s2) : 0;
 	size_t length_result = length_s1 + length_s2 + 1;
 
 	char *result = malloc(length_result);
