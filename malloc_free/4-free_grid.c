d3c51ddf74b9195e5e168cfbf6e1253ae881872f
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-28 12:27:27 -0700

    0

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - frees the previously allocated grid
+ *
+ * Return: (nothing)
+ */
+
+void free_grid(int **grid, int height);
+{
+	for (i = 0; i < height; i++)
+	{
+	free(grid);
+	}
+
+	free(grid);
+}
