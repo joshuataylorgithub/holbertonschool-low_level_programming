cc59240877961c834b5ad60f9d57c8c82f78dc87
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-29 19:24:37 -0700

    0

---
+++ preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ *
+ *
+ * Return: (0)
+ */
+int main()
+{
+	int array[SIZE];
+
+	printf("Size of array: %lu\n", sizeof(array));
+
+	return (0);
+}
