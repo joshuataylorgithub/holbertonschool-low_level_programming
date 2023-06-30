8b6d352b41a06b36a11d2de4062a6f820656130b
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-29 20:56:06 -0700

    0

---
+++ preprocessor/4-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "4-sum.h"
+/*
+ * main - checks the code
+ *
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int num1 = 10;
+	int num2 = 20;
+	int result = SUM(num1, num2);
+
+	printf("Sum of %d and %d is %d\n", num1, num2, result);
+	return (0);
+}
