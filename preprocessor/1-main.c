32fc71ec80c95875c48bd3060df346d70939bc9b
Joshua Taylor <joshuataylor7777@gmail.com>
2023-06-29 19:40:14 -0700

    0

---
+++ preprocessor/1-main.c
@@ -0,0 +1,17 @@
+#include "1-pi.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ *
+ *
+ * Return: (0)
+ */
+
+int main()
+{
+	double radius = 5.0;
+	double circumference = 2 * PI * radius;
+
+	printf("Circumference: %.2f\n", circumference);
+	return (0);
+}
