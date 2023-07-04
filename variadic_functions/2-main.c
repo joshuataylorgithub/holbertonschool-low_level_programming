f2b31641604ca903b9956b5ffe5424647d023b7d
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-04 11:07:20 -0700

    0

---
+++ variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - a function that prints strings followed by a new line
+ * @seperator: the string(s) to be printed
+ * @n: number of arguments in the function
+ */
+
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	const char *str;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+	str = va_arg(args, const char *);
+
+	if (str == NULL)
+	printf("(nil)");
+	
+	else
+	printf("%s", str);
+
+	if (seperator == NULL && i < n - 1)
+	printf("%s", seperator);
+	}
+	va_end(args);
+	printf("\n");
+}
