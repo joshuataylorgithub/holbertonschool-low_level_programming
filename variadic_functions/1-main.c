4a2e2fd134cf6a91928d85484e1a5db55efdfcd9
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-04 10:44:32 -0700

    0

---
+++ variadic_functions/1-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * main - checks the code
+ *
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	print_numbers(", ", 4, 98, -1024, 402);
+	return (0);
+}
---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", va_arg(args,int));
+
+	if (seperator != NULL && i < n - 1)
+	printf("%s", seperator);
+	}
+
+	va_end(args);
+	printf("\n");
+}
