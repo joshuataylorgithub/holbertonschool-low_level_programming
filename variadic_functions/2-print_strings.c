0e266b0cd45053acb367c17e64b2f2e69befa786
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-04 21:12:23 -0700

    5

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,19 +17,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	const char *next_str;
 
 	va_start(args, n);
+	
+	str = va_arg(args, const char*);
 	for (i = 0; i < n; i++)
 	{
-	str = va_arg(args, const char *);
-
 	if (str == NULL)
 	printf("(nil)");
 	
 	else
 	printf("%s", str);
 
+	next_str = va_arg(args, const char *);
 	if (separator != NULL && i < n - 1 && str != NULL)
 	{
-	next_str = va_arg(args, const char *);
 	if (next_str != NULL)
 	printf("%s", separator);
 	}
