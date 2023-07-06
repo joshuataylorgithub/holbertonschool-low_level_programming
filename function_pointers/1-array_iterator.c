7f993bb1709579b8d8e9363b91c577d75d6027ed
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-05 21:21:10 -0700

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,6 +4,7 @@
  * array_iterator - pointer to a function that prints arrays
  * @array: array to print
  * @size: size of array
+ * @action: ptr to a function that takes 'int' parameter and returns void
  * Return: void
  */
 
@@ -11,9 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	
+
 	if (array != NULL && action != NULL)
-	
+
 	{
 	for (i = 0; i < size; i++)
 	{
