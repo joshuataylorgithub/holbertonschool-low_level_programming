19f71c20b14ee6dc68ee37b99793fe8c8ae8009a
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-06 00:29:07 -0700

    0

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,14 +4,17 @@
 /**
  * print_dog - prints the content of a struct_dog
  * @d: pointer to the struct_dog to point
- */
+ * struct_dog - dog information
+ *
+ * /
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
+	if (d == NULL)
+	return (NULL);
+
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.2f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %d\n", (d->owner != NULL) ? d->name : "(nil)");
-	}
+
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,5 +10,6 @@
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
