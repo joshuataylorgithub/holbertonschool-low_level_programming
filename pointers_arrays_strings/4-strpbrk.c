a9154530fb5bb1a99c1b185550482508185a88bc
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-05 23:19:39 -0700

    0

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes
+ * @s: searches string 's'
+ * @a: ptr to a pointer *accept
+ * Return: (s)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *a = accept;
+
+	while (*s != '\0')
+	{
+	while (*a != '\0')
+	{
+	if (*s == *a)
+	{
+	return (s);
+	}
+	a++;
+	}
+	s++;
+	}
+	return (NULL);
+}
+
