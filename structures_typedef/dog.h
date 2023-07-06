af9b373c395e2f966f65ef906518fbb5a7a862a0
Joshua Taylor <joshuataylor7777@gmail.com>
2023-07-06 01:01:35 -0700

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+typedef struct dog dog_t;
 {
 	char *owner;
 	float age;
 	char *name;
 }
 
-typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
